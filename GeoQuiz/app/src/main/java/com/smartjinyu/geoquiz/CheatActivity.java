001b09d5cf346e661f44ec6a8a92053b7d2b9cd4
smartjinyu <smartjinyu@gmail.com>
2016-10-29 21:49:45 +0800

    update to Chapter 6

--- GeoQuiz/app/src/main/java/com/smartjinyu/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/smartjinyu/geoquiz/CheatActivity.java
@@ -1,10 +1,13 @@
 package com.smartjinyu.geoquiz;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -43,6 +46,32 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnwerShownResult(true);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animator) {}
+                        @Override
+                        public void onAnimationEnd(Animator animator) {
+                            //super.onAnimationEnd(animator);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                        @Override
+
+                        public void onAnimationCancel(Animator animator) {}
+                        @Override
+                        public void onAnimationRepeat(Animator animator) {}
+                    });
+                    anim.start();
+                }else{
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
