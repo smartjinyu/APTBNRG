157434e147bf3b608cdb275e14ef0f8e6d290a7d
smartjinyu <smartjinyu@gmail.com>
2016-11-22 00:12:51 +0800

    update to Chapter 12

--- CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/CrimeFragment.java
@@ -1,6 +1,8 @@
 package com.smartjinyu.criminalintent;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,6 +17,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import java.net.DatagramPacket;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -25,6 +28,7 @@ public class CrimeFragment extends Fragment{
     private Crime mCrime;
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
 
     private EditText mTitleField;
     private CheckBox mSolvedCheckBox;
@@ -68,12 +72,13 @@ public class CrimeFragment extends Fragment{
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        updateDate();
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);//so DatePickerFragment's Target fragment is CrimeFragement
                 dialog.show(manager,DIALOG_DATE);
             }
         });
@@ -88,6 +93,22 @@ public class CrimeFragment extends Fragment{
         });
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult (int requestCode,int resultCode,Intent data){
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
     }
 
 }
--- CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/DatePickerFragment.java
@@ -1,8 +1,10 @@
 package com.smartjinyu.criminalintent;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by smartjinyu on 2016/11/2.
@@ -18,6 +21,8 @@ import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
+    public static final String EXTRA_DATE = "com.smartjinyu.criminalintent.date";
+
     private DatePicker mDatePicker;
 
     public static DatePickerFragment newInstance(Date date){
@@ -46,7 +51,25 @@ public class DatePickerFragment extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                     .setView(v)
                     .setTitle(R.string.date_picker_title)
-                    .setPositiveButton(android.R.string.ok,null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            int year = mDatePicker.getYear();
+                            int month = mDatePicker.getMonth();
+                            int day = mDatePicker.getDayOfMonth();
+                            Date date = new GregorianCalendar(year,month,day).getTime();
+                            sendResult(Activity.RESULT_OK,date);
+                        }
+                    })
                     .create();
+    }
+
+    private void sendResult(int resultCode,Date date){
+        if(getTargetFragment() == null){
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE,date);
+        getTargetFragment().onActivityResult(getTargetRequestCode(),resultCode,intent);
     }
 }
