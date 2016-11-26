b4b24c7737b48898dcbb79c2d2e86cdf07a2006f
smartjinyu <smartjinyu@gmail.com>
2016-11-26 11:14:55 +0800

    finish the first challenge in Chapter 15

--- CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/smartjinyu/criminalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -110,12 +111,10 @@ public class CrimeFragment extends Fragment{
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT,getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.crime_report_subject));
-                i = Intent.createChooser(i,getString(R.string.send_report));
-                startActivity(i);
+                ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder.from(getActivity());
+                intentBuilder.setType("text/plain");
+                intentBuilder.setText(getCrimeReport());
+                intentBuilder.startChooser();
             }
         });
 
