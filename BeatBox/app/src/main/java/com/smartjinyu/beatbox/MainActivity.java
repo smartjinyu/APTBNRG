ebfa2d678c863edd080d136381dafc8366cea0c3
smartjinyu <smartjinyu@gmail.com>
2016-12-19 23:37:57 +0800

    create new project BeatBox

---
+++ BeatBox/app/src/main/java/com/smartjinyu/beatbox/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smartjinyu.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
