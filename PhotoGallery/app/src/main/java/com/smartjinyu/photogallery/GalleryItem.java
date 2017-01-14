f0136d253d7e0334712d44e6d1abdd7c934073cc
smartjinyu <smartjinyu@gmail.com>
2017-01-15 00:21:38 +0800

    finish Chapter 23

---
+++ PhotoGallery/app/src/main/java/com/smartjinyu/photogallery/GalleryItem.java
@@ -0,0 +1,41 @@
+package com.smartjinyu.photogallery;
+
+/**
+ * Created by smartjinyu on 2016/12/24.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    @Override
+    public String toString(){
+        return mCaption;
+
+    }
+}
