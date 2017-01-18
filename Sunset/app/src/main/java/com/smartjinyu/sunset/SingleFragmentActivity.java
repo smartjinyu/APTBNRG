e113d05cdc2796c94be1a004f008d4da9839691a
smartjinyu <smartjinyu@gmail.com>
2017-01-18 16:02:24 +0800

    finish Chapter 30

---
+++ Sunset/app/src/main/java/com/smartjinyu/sunset/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.smartjinyu.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by smartjinyu on 2017/1/17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
+    }
+}
